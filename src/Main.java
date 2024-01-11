a4ba3095610fc933683ba1d3e81b6bf44ef32530
Thomas Aussenac <thomas.aussenac@etu.umontpellier.fr>
2024-01-11 12:00:02 +0100

    2 premier algo fini (à vérifier)

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,36 @@ public class Main{
         }
     }
 
+    public static int rechercheSansRupture(TNP pfTNPEtu, Etudiant pfEtudiant){
+        int traceI=0;
+        int indice=-1;
+        for(int i=0;i<pfTNPEtu.nbElt;i++){
+            if(pfTNPEtu.lesElements[i].equals(pfEtudiant))
+                indice=i;
+            traceI++;
+        }
+        System.out.println("Nous avons parcouru "+traceI+" éléments de la liste pour trouver l'élément recherché.");
+        return indice;
+    }
+    public static int rechercheAvecRupture(TNP pfTNPEtu, Etudiant pfEtudiant){
+        int traceI=0;
+        int indice=-1;
+        for(int i=0;i<pfTNPEtu.nbElt;i++){
+            if(pfTNPEtu.lesElements[i].equals(pfEtudiant)){
+                indice=i;
+                traceI++;
+                break;
+            }
+            traceI++;
+        }
+        System.out.println("Nous avons parcouru "+traceI+" éléments de la liste pour trouver l'élément recherché.");
+        return indice;
+    }
+    public static int rechercheDichotomie(TNP pfTNPEtu, Etudiant pfEtudiant){
+
+        return 0;
+    }
+
 
 
 
