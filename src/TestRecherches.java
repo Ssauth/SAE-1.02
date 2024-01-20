6f21d89756792d626408eeab331b693022f0eb0e
Thomas PC fixe <thomas.aussenac@etu.umontpellier.fr>
2024-01-20 02:58:51 +0100

    Tout les tests fait et passés

--- src/TestRecherches.java
+++ src/TestRecherches.java
@@ -4,13 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestRecherches {
 
-    private TNP liste = new TNP(250);
+    private TNP liste1 = new TNP(250);
+    private TNP liste2 = new TNP(250);
+    private TNP liste3 = new TNP(10000);
+    private TNP liste4 = new TNP(100000);
 
     @BeforeAll
     public void setUp(){
         try {
-            Main.getListe("listenomssansaccent.csv", liste);
-            Main.trierEtu(liste);
+            Main.getListe("listenomssansaccent.csv", liste1);
+            Main.trierEtu(liste1);
+            Main.getListe("listenomsavecaccent.csv", liste2);
+            Main.trierEtu(liste2);
+            Main.getListe("listenomsXL_10k.csv", liste3);
+            Main.trierEtu(liste3);
+            Main.getListe("listenomsXXL_100k.csv", liste4);
+            Main.trierEtu(liste4);
+
         } catch (Exception e) {
             System.out.println("Erreur : " + e.getMessage());
         }
@@ -21,23 +31,51 @@ public class TestRecherches {
 
     @Test
     public void testRechercheSansRupture(){
-        for (Etudiant e : liste.lesElements)
+        for (Etudiant e : liste1.lesElements)
             if (e!=null)
-                assertEquals(Main.rechercheSansRupture(liste,e.prenom,e.nom).etudiant,e);
+                assertEquals(Main.rechercheSansRupture(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste2.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheSansRupture(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste3.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheSansRupture(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste4.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheSansRupture(liste1,e.prenom,e.nom).etudiant,e);
+
     }
 
     @Test
     public void testRechercheAvecRupture(){
-        for (Etudiant e : liste.lesElements)
+        for (Etudiant e : liste1.lesElements)
             if (e!=null)
-                assertEquals(Main.rechercheSansRupture(liste,e.prenom,e.nom).etudiant,e);
+                assertEquals(Main.rechercheAvecRupture(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste2.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheAvecRupture(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste3.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheAvecRupture(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste4.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheAvecRupture(liste1,e.prenom,e.nom).etudiant,e);
     }
 
     @Test
     public void testRechercheDichotomie(){
-        for (Etudiant e : liste.lesElements)
+        for (Etudiant e : liste1.lesElements)
             if (e!=null)
-                assertEquals(Main.rechercheSansRupture(liste,e.prenom,e.nom).etudiant,e);
+                assertEquals(Main.rechercheDichotomie(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste2.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheDichotomie(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste3.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheDichotomie(liste1,e.prenom,e.nom).etudiant,e);
+        for (Etudiant e : liste4.lesElements)
+            if (e!=null)
+                assertEquals(Main.rechercheDichotomie(liste1,e.prenom,e.nom).etudiant,e);
     }
 
 
